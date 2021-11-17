a38c9d79c76cb6307155cc88c858716f570fac64
Emma Leonardi <eleonardi.mobile@gmail.com>
2021-11-17 20:10:54 +0100

    Finished SimpleGUI

--- src/it/unibo/oop/lab/mvcio/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvcio/SimpleGUI.java
@@ -1,9 +1,16 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -11,7 +18,8 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final String TITLE = "My first Java graphical interface";
+    private final JFrame frame = new JFrame(TITLE);
 
     /*
      * Once the Controller is done, implement this class in such a way that:
@@ -37,26 +45,59 @@ public final class SimpleGUI {
      * builds a new {@link SimpleGUI}.
      */
     public SimpleGUI() {
+
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextArea box = new JTextArea();
+        canvas.add(box, BorderLayout.CENTER);
+        final JButton save = new JButton("Save on file");
+        canvas.add(save, BorderLayout.SOUTH);
+        save.addActionListener(new ActionListener() {
+            @Override
+            /**
+             * Saves the content of the Text Area in the file
+             */
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    new Controller().writeOnFile(box.getText());
+                } catch (final IOException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+        });
+
         /*
-         * Make the frame half the resolution of the screen. This very method is
-         * enough for a single screen setup. In case of multiple monitors, the
-         * primary is selected.
+         * Make the frame half the resolution of the screen. This very method is enough
+         * for a single screen setup. In case of multiple monitors, the primary is
+         * selected.
          * 
-         * In order to deal coherently with multimonitor setups, other
-         * facilities exist (see the Java documentation about this issue). It is
-         * MUCH better than manually specify the size of a window in pixel: it
-         * takes into account the current resolution.
+         * In order to deal coherently with multimonitor setups, other facilities exist
+         * (see the Java documentation about this issue). It is MUCH better than
+         * manually specify the size of a window in pixel: it takes into account the
+         * current resolution.
          */
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
         frame.setSize(sw / 2, sh / 2);
         /*
-         * Instead of appearing at (0,0), upper left corner of the screen, this
-         * flag makes the OS window manager take care of the default positioning
-         * on screen. Results may vary, but it is generally the best choice.
+         * Instead of appearing at (0,0), upper left corner of the screen, this flag
+         * makes the OS window manager take care of the default positioning on screen.
+         * Results may vary, but it is generally the best choice.
          */
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationByPlatform(true);
+        frame.setVisible(true);
     }
 
+    /**
+     * @param args
+     *                 ignored
+     */
+    public static void main(final String... args) {
+        new SimpleGUI();
+
+    }
 }
