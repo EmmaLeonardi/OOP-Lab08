e0c5f103346afdc5c9a08b8109e54f58db27eafc
Emma Leonardi <eleonardi.mobile@gmail.com>
2021-11-27 15:24:16 +0100

    Removed old comment

--- src/it/unibo/oop/lab/advanced/ResourceLoader.java
+++ src/it/unibo/oop/lab/advanced/ResourceLoader.java
@@ -7,26 +7,6 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ResourceLoader {
-    /*
-     * minimum, maximum, and number of allowed attempts are hard-coded Modify the
-     * application in such a way that those settings are read from the supplied
-     * configuration files (config.yml). An example of configuration file is
-     * available in the res folder.
-     * 
-     * Do not make the View operate on the file system. More precisely, the
-     * controller in its constructor must be able to read the provided configuration
-     * file, and import the three constants. Even better, an external utility
-     * (either a method or another class) could be responsible for that. The reason
-     * is that the interaction with the file system is not part of the domain model,
-     * nor, in this case, is part of the view.
-     * 
-     * You can use StringTokenizer to split a String in parts. Another option,
-     * available for this very case, is String.split(), passing ":" as argument.
-     * Beware of split(): the input String is actually a regular expression. Regular
-     * expressions (regex) are a very powerful tool, but we can not cover them in
-     * this course: StringTokenizer is recommended unless you already know regular
-     * expressions and how to use them.
-     */
 
     private int min;
     private int max = 100;
